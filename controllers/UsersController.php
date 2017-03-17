1fbcbf41aa70d27ba5855f2dd90d416719388479
gabrieldornianu <gabrieldornianu@yahoo.com>
2017-03-17 12:23:07 +0200

    Added the mailing function

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -20,6 +20,24 @@ class UsersController extends \yii\web\Controller
             $model->cv_file = UploadedFile::getInstance($model, 'cv_file');
             $model->image_file = UploadedFile::getInstance($model, 'image_file');
             if($model->validate()){
+                
+                Yii::$app->mailer->compose()
+                ->setFrom($model->email)
+                ->setTo("gabrieldornianu@yahoo.com")
+                ->setSubject("New rotaract request")
+                ->setTextBody(
+                "Nume: ".$model->name
+                ."\nPrenume: ".$model->surname
+                ."\nTelefon".$model->phone
+                ."\nEmail".$model->email
+                ."\nDespre mine: ".$model->description
+                ."\nMotivul pentru care ma inscriu: ".$model->reason
+                ."\nPozitia in cadrul proiectului: ".$model->position
+                )
+                ->attach($model->cv)
+                ->attach($model->image)
+                ->send();
+
                 Yii::$app->session->setFlash('success', 'Thanks for submitting. We will respond in no time!');
                 $model->cv_file->saveAs('uploads/'.$model->cv_file->baseName.'.'.$model->cv_file->extension);
                 $model->image_file->saveAs('uploads/'.$model->image_file->baseName.'.'.$model->image_file->extension);
